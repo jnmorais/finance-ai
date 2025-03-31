70ee5c1730dbe4d98572b873c3809aff849d85b6
jean <jsmorais2002@gmail.com>
2025-03-31 16:23:06 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const Subscripition = () => {
+  return <h1>Inscrições</h1>;
+};
+
+export default Subscripition;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transactions = () => {
+  return <h1>Transações</h1>;
+};
+
+export default Transactions;
