7ebc7ffb3fd4fced01f329664cf66407b59501b0
jean <jsmorais2002@gmail.com>
2025-04-01 14:23:59 -0300

    chore: add shadcn setup

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../_components/ui/button";
+
 const Subscripition = () => {
   return (
     <div className="flex w-screen items-center justify-center">
       <h1 className="text-red-400">Inscrições</h1>
+      <Button>Oi</Button>
     </div>
   );
 };
