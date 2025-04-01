84169feabe1990104715bb01ed4af05bbc3a42d6
jean <jsmorais2002@gmail.com>
2025-04-01 16:23:30 -0300

    feat: add authetication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>Oi</h1>;
-}
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
+};
+
+export default Home;
