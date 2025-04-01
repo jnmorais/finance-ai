8c80466a0bee7537328aa92e75152b3381bf1f99
jean <jsmorais2002@gmail.com>
2025-04-01 14:01:08 -0300

    chore: add tailwind prettier plugin

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,9 @@
 const Subscripition = () => {
-  return <h1>Inscrições</h1>;
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="text-red-400">Inscrições</h1>
+    </div>
+  );
 };
 
 export default Subscripition;
