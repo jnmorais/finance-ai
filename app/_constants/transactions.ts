fe17053776fc998e73cafa2a43ec58b4fbb4666b
jnmorais <jsmorais2002@gmail.com>
2025-04-04 23:38:49 -0300

    feat: create add transaction dialog

---
+++ app/_constants/transactions.ts
@@ -0,0 +1,112 @@
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAIMENT: "Entreterimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salário",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENTS_LABELS = {
+  CREDIT_CARD: "Cartão de Crédito",
+  DEBIT_CARD: "Cartão de Débito",
+  BANK_TRANSFER: "Transferência Bancária",
+  BANK_SLIP: "Boleto Bancário",
+  CASH: "Dinheiro",
+  PIX: "Pix",
+  OTHER: "Outros",
+};
+
+export const TRANSACTION_TYPE_OPTIONS = [
+  {
+    value: TransactionType.DEPOSIT,
+    label: "Deposito",
+  },
+  {
+    value: TransactionType.EXPENSE,
+    label: "Despesa",
+  },
+  {
+    value: TransactionType.INVESTMENT,
+    label: "Investimento",
+  },
+];
+
+export const TRANSACTION_PAYMENT_METHOD_OPTIONS = [
+  {
+    value: TransactionPaymentMethod.CREDIT_CARD,
+    label: TRANSACTION_PAYMENTS_LABELS[TransactionPaymentMethod.CREDIT_CARD],
+  },
+  {
+    value: TransactionPaymentMethod.DEBIT_CARD,
+    label: TRANSACTION_PAYMENTS_LABELS[TransactionPaymentMethod.DEBIT_CARD],
+  },
+  {
+    value: TransactionPaymentMethod.BANK_TRANSFER,
+    label: TRANSACTION_PAYMENTS_LABELS[TransactionPaymentMethod.BANK_TRANSFER],
+  },
+  {
+    value: TransactionPaymentMethod.BANK_SLIP,
+    label: TRANSACTION_PAYMENTS_LABELS[TransactionPaymentMethod.BANK_SLIP],
+  },
+  {
+    value: TransactionPaymentMethod.CASH,
+    label: TRANSACTION_PAYMENTS_LABELS[TransactionPaymentMethod.CASH],
+  },
+  {
+    value: TransactionPaymentMethod.PIX,
+    label: TRANSACTION_PAYMENTS_LABELS[TransactionPaymentMethod.PIX],
+  },
+  {
+    value: TransactionPaymentMethod.OTHER,
+    label: TRANSACTION_PAYMENTS_LABELS[TransactionPaymentMethod.OTHER],
+  },
+];
+
+export const TRANSACTION_CATEGORY_OPTIONS = [
+  {
+    value: TransactionCategory.EDUCATION,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.EDUCATION],
+  },
+  {
+    value: TransactionCategory.ENTERTAIMENT,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.ENTERTAIMENT],
+  },
+  {
+    value: TransactionCategory.FOOD,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.FOOD],
+  },
+  {
+    value: TransactionCategory.HEALTH,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.HEALTH],
+  },
+  {
+    value: TransactionCategory.HOUSING,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.HOUSING],
+  },
+  {
+    value: TransactionCategory.OTHER,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.OTHER],
+  },
+  {
+    value: TransactionCategory.SALARY,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.SALARY],
+  },
+  {
+    value: TransactionCategory.TRANSPORTATION,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.TRANSPORTATION],
+  },
+  {
+    value: TransactionCategory.UTILITY,
+    label: TRANSACTION_CATEGORY_LABELS[TransactionCategory.UTILITY],
+  },
+];
--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -5,28 +5,10 @@ import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
 import { Button } from "@/app/_components/ui/button";
 import { PencilIcon, TrashIcon } from "lucide-react";
-
-const TRANSACTION_CATEGORY_LABELS = {
-  EDUCATION: "Educação",
-  ENTERTAIMENT: "Entretenimento",
-  FOOD: "Alimentação",
-  HEALTH: "Saúde",
-  HOUSING: "Moradia",
-  OTHER: "Outros",
-  SALARY: "Salário",
-  TRANSPORTATION: "Transporte",
-  UTILITY: "Utilidades",
-};
-
-const TRANSACTION_PAYMENTS_LABELS = {
-  CREDIT_CARD: "Cartão de Crédito",
-  DEBIT_CARD: "Cartão de Dебito",
-  BANK_TRANSFER: "Transferência Bancária",
-  BANK_SLIP: "Boleto Bancário",
-  CASH: "Dinheiro",
-  PIX: "Pix",
-  OTHER: "Outros",
-};
+import {
+  TRANSACTION_CATEGORY_LABELS,
+  TRANSACTION_PAYMENTS_LABELS,
+} from "@/app/_constants/transactions";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
