912cdd6f412d3687499ad4d16644c8a05fb52330
jean <jsmorais2002@gmail.com>
2025-04-07 16:12:27 -0300

    feat: add summary cards

---
+++ app/(home)/_components/time-select.tsx
@@ -0,0 +1,52 @@
+"use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/app/_components/ui/select";
+import { SelectValue } from "@radix-ui/react-select";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const MONTH_OPTIONS = [
+  { value: "01", label: "Jan" },
+  { value: "02", label: "Fevereiro" },
+  { value: "03", label: "Março" },
+  { value: "04", label: "Abril" },
+  { value: "05", label: "Maio" },
+  { value: "06", label: "Junho" },
+  { value: "07", label: "Julho" },
+  { value: "08", label: "Agosto" },
+  { value: "09", label: "Setembro" },
+  { value: "10", label: "Outubro" },
+  { value: "11", label: "Novembro" },
+  { value: "12", label: "Dezembro" },
+];
+
+const TimeSelect = () => {
+  const { push } = useRouter();
+  const searchParams = useSearchParams();
+  const month = searchParams.get("month");
+  const handleMonthChange = (month: string) => {
+    push(`/?month=${month}`);
+  };
+  return (
+    <Select
+      onValueChange={(value) => handleMonthChange(value)}
+      defaultValue={month ?? ""}
+    >
+      <SelectTrigger className="w-[150px]">
+        <SelectValue placeholder="Mês"></SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        {MONTH_OPTIONS.map((option) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default TimeSelect;
