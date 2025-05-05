f7988f6aea10af3cd17695e5106ace78387900d7
jean <jsmorais2002@gmail.com>
2025-05-05 00:20:56 -0300

    refactor: ensure responsive dialog in aiReportButton

--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -57,7 +57,7 @@ const AiReportButton = ({ month, hasPremiumPlan }: AiReportButtonProps) => {
           <BotIcon />
         </Button>
       </DialogTrigger>
-      <DialogContent className="max-w-[600px]">
+      <DialogContent className="max-w-[600px] max-[840px]:w-[80%]">
         {hasPremiumPlan ? (
           <>
             <DialogHeader>
