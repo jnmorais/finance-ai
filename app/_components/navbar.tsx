4d7b22c890034f4ffc1d82e43752fedcaa4f3ce8
jean <jsmorais2002@gmail.com>
2025-05-05 00:28:05 -0300

    feat: add link to homepage on logo image

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -15,7 +15,9 @@ const Navbar = () => {
     <nav className="flex justify-between border-b border-solid px-8 py-4">
       {/* ESQUERDA */}
       <div className="flex items-center gap-10">
-        <Image src="/logo.svg" width={173} height={39} alt="Finance AI" />
+        <Link href="/">
+          <Image src="/logo.svg" width={173} height={39} alt="Finance AI" />
+        </Link>
         <div className="flex items-center gap-10 max-md:hidden">
           <Link
             href="/"
