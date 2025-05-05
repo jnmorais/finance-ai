3b5cf2689f76f127d7a4ecc78385813f30815247
jean <jsmorais2002@gmail.com>
2025-05-04 23:55:17 -0300

    refactor: ensure responsive login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
 import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -10,36 +10,38 @@ const LoginPage = async () => {
   if (userId) {
     redirect("/");
   }
+
   return (
-    <div className="grid h-full grid-cols-2">
-      {/* ESQUERDA */}
+    <div className="grid h-screen grid-cols-2 max-md:grid-cols-1">
+      {/* Left side */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
           src="/logo.svg"
-          alt="logo"
-          width={173}
+          alt="Finance AI"
+          width={163}
           height={39}
           className="mb-8"
         />
-
-        <h1 className="mb-3 text-4xl font-bold">Bem-vindo </h1>
-        <p className="text-muted-foreground mb-8">
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
+
         <SignInButton>
-          <Button variant="outline" className="mr-2">
-            <LogInIcon />
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
             Fazer login ou criar conta
           </Button>
         </SignInButton>
       </div>
-      {/* DIREITA */}
-      <div className="relative h-full w-full">
+
+      {/* Right side */}
+      <div className="relative h-full w-full max-md:hidden">
         <Image
           src="/login.png"
-          alt="Faça login"
+          alt="Faça Login"
           fill
           className="object-cover"
         />
